22fb23b64d30c0e6ce1e02b90656233ad3dac8ee
Balamurugan <balamuruganpgb08@gmail.com>
2021-05-06 09:39:20 +0800

    Change Table & Column Name

--- index.php
+++ index.php
@@ -3,86 +3,87 @@ $conn = mysqli_connect("localhost", "root", "", "blognews");
 
 $affectedRow = 0;
 
-$xml = simplexml_load_file("goodnewspost.xml") or die("Error: Cannot create object");
+$xml = simplexml_load_file("goodnewsimage.xml") or die("Error: Cannot create object");
 
 
 foreach ($xml->xpath('//item') as $item) {
 
-    // $thumbnail = $item->link;
-    // $post_id = $item->post_id;
+    $thumbnail = $item->guid;
+    $post_id = $item->post_parent;
 
-    //  $sqlupdatepost =  "UPDATE tbl_tutorials SET thumbnail='".$thumbnail."' WHERE id='".$post_id."'";
+     $sqlupdatepost =  "UPDATE posts SET thumbnail='".$thumbnail."' WHERE id='".$post_id."'";
 
-    //  $result4 = mysqli_query($conn, $sqlupdatepost);
+     $result4 = mysqli_query($conn, $sqlupdatepost);
     
-    $convertContent = addslashes($item->content);
+    // $convertContent = addslashes($item->content);
 
-    if ($item->status == 'publish') {
-        $is_published = 1;
-    }else{
-        $is_published = 0;
-    }
+    // if ($item->status == 'publish') {
+    //     $is_published = 1;
+    // }else{
+    //     $is_published = 0;
+    // }
 
-    $sqlgetid =  "SELECT id FROM users WHERE name = '".$item->creator."'";
+    // $sqlgetid =  "SELECT id FROM users WHERE name = '".$item->creator."'";
 
-    $get_id = $conn->query($sqlgetid);
-    $rowcount=mysqli_num_rows($get_id);
-    $result_assoc = $get_id->fetch_assoc();
-
-    foreach ($result_assoc as $user_id) {
-
-        $title = $item->title;
-        $post_type = $item->post_type;
-        $post_id = $item->post_id;
-        $link = $item->link;
-        $pubDate = $item->pubDate;
-        $user_id =   $user_id;
-        $content =  $convertContent;
-        $is_published1 =  $is_published;
-        $slug = $item->post_name;
-        $mainModule_id = 1;//goodnews = 1, bac360 = 2
-        $category_name = $item->category['nicename'];
-
+    // $get_id = $conn->query($sqlgetid);
+    // $rowcount=mysqli_num_rows($get_id);
+    // $result_assoc = $get_id->fetch_assoc();
 
     
-        $sql = "INSERT INTO posts(id,mainModule_id,title,slug,link,pubDate,user_id,content,post_type,is_published,category) VALUES ('" . $post_id . "','" . $mainModule_id . "','" . $title . "','" . $slug . "','" . $link . "','" . $pubDate . "','" . $user_id . "','" . $content . "','" . $post_type . "','" . $is_published1 . "','" . $category_name . "')"; 
+    // foreach ($result_assoc as $user_id) {
 
-        // $sql2 = "INSERT INTO category_tbl(mainmodule_id,category_name,category_slug,is_published,user_id) VALUES ('" . $mainModule_id ."','" . $category_name ."','" . $category_name ."','" . $is_published1 ."','" . $user_id ."')";
+    //     $title = $item->title;
+    //     $post_type = $item->post_type;
+    //     $post_id = $item->post_id;
+    //     $link = $item->link;
+    //     $pubDate = $item->pubDate;
+    //     $user_id =   $user_id;
+    //     $content =  $convertContent;
+    //     $is_published1 =  $is_published;
+    //     $slug = $item->post_name;
+    //     $mainModule_id = 1;//goodnews = 1, bac360 = 2
+    //     $category_name = $item->category['nicename'];
 
-        // $sqlgetcategoryid =  "SELECT id FROM category_tbl WHERE category_name = '".$category_name."'";
 
-        // $get_categoryid = $conn->query($sqlgetcategoryid);
-        // $rowcount2=mysqli_num_rows($get_categoryid);
-        // $result_assoc2 = $get_categoryid->fetch_assoc();
+    
+    //     // $sql = "INSERT INTO posts(id,mainModule_id,title,slug,created_at,user_id,details,post_type,is_published) VALUES ('" . $post_id . "','" . $mainModule_id . "','" . $title . "','" . $slug . "','" . $pubDate . "','" . $user_id . "','" . $content . "','" . $post_type . "','" . $is_published1 . "')"; 
 
-        // foreach ($result_assoc2 as $category_id) {
+    //     // $sql2 = "INSERT INTO categories(mainmodule_id,category_name,category_slug,is_published,user_id) VALUES ('" . $mainModule_id ."','" . $category_name ."','" . $category_name ."','" . $is_published1 ."','" . $user_id ."')";
 
-        //     $cat_id = $category_id;
+    //     $sqlgetcategoryid =  "SELECT id FROM categories WHERE category_name = '".$category_name."'";
 
-        //      $sql3 = "INSERT INTO category_posts(category_id,post_id) VALUES ('" . $cat_id ."','" . $post_id ."')";
+    //     $get_categoryid = $conn->query($sqlgetcategoryid);
+    //     $rowcount2=mysqli_num_rows($get_categoryid);
+    //     $result_assoc2 = $get_categoryid->fetch_assoc();
 
-        //      $result3 = mysqli_query($conn, $sql3);
+    //     foreach ($result_assoc2 as $category_id) {
 
-        // }
+    //         $cat_id = $category_id;
+
+    //          $sql3 = "INSERT INTO category_posts(category_id,post_id) VALUES ('" . $cat_id ."','" . $post_id ."')";
+
+    //          $result3 = mysqli_query($conn, $sql3);
+
+    //     }
 
        
 
 
         
-        $result = mysqli_query($conn, $sql);
-        // $result2 = mysqli_query($conn, $sql2);
+    //     // $result = mysqli_query($conn, $sql);
+    //     // $result2 = mysqli_query($conn, $sql2);
 
 
 
 
         
-        if (! empty($result)) {
+        if (! empty($result4)) {
             $affectedRow ++;
         } else {
             $error_message = mysqli_error($conn) . "\n";
         }
        
-    }
+    // }
 
   
 
@@ -99,9 +100,9 @@ foreach ($xml->xpath('//item') as $item) {
 foreach ($xml->xpath('//author') as $author) {
 
 
-   //    // echo $child ."\n";
+      // echo $child ."\n";
    //   $author_id = $author->author_id;
-   //  $author_name = $author->author_display_name;
+   //  $author_name = $author->author_login;
    //  $author_email = $author->author_email;
    //  $author_pass = password_hash("bac@2021", PASSWORD_DEFAULT);
    //  $author_role = 'Editor';
